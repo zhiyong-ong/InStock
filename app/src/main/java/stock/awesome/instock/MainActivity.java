0c2a90e3d121bfa3a72b73ea6bbbf58d35c75ad7
kabir-plod <kabirk@live.com>
2016-01-29 23:34:05 +0800

    Test

--- app/src/main/java/stock/awesome/instock/MainActivity.java
+++ app/src/main/java/stock/awesome/instock/MainActivity.java
@@ -16,20 +16,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Firebase.setAndroidContext(this);
-        Firebase myFirebaseRef = new Firebase("https://scorching-inferno-2190.firebaseio.com/");
+        Firebase database = new Firebase("https://scorching-inferno-2190.firebaseio.com/");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
