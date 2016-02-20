c8b82b19c507b70fc660ac5c6ca090a177681d72
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-02-20 15:32:56 +0800

    changed

--- app/src/main/java/stock/awesome/instock/BuildKit.java
+++ app/src/main/java/stock/awesome/instock/BuildKit.java
@@ -1,10 +1,17 @@
 package stock.awesome.instock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class BuildKit extends AppCompatActivity {
 
@@ -14,6 +21,7 @@ public class BuildKit extends AppCompatActivity {
         setContentView(R.layout.activity_build_kit);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
 
     }
@@ -29,5 +37,43 @@ public class BuildKit extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendNewKitItem(View view) {
+        showChangeLangDialog();
+    }
+
+    public void showChangeLangDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.popup_add_item, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText productID = (EditText) dialogView.findViewById(R.id.productID);
+        final EditText quantity = (EditText) dialogView.findViewById(R.id.addQuantity);
+        //dialogBuilder.setTitle("Input New Item");
+        //dialogBuilder.setMessage("Enter text below");
+        dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                populateListView(productID.toString(), quantity.toString());
+                //do something with edt.getText().toString();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+    public void populateListView(String productID, String quantity) {
+        ListView mainListView = (ListView) findViewById( R.id.listView );
+        ArrayAdapter<String> listAdapter;
+        listAdapter = new ArrayAdapter<String>(this, R.layout.listview_row);
+        listAdapter.add(productID + "\t" + quantity);
+        mainListView.setAdapter(listAdapter);
+
     }
 }
