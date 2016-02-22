5d5aabd60dc312706a51c6d7dccd74cc7df0e70f
kabir-plod <kabirk@live.com>
2016-02-22 15:06:01 +0800

    InputStockActivity returns products with attributes entered

--- app/src/main/java/stock/awesome/instock/MainActivity.java
+++ app/src/main/java/stock/awesome/instock/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        
     }
 
     public void sendNewItemIntent(View view) {
