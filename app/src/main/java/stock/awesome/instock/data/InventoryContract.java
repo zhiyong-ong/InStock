a470407dfde1417a30f11a98209df7dbe16432fd
kabir-plod <kabirk@live.com>
2016-03-28 21:55:45 +0800

    Creating local sqlite database of items and collections (products and kits)

--- app/src/main/java/stock/awesome/instock/data/InventoryContract.java
+++ app/src/main/java/stock/awesome/instock/data/InventoryContract.java
@@ -5,6 +5,8 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.util.GregorianCalendar;
+
 
 /**
  * Defines table and column names for the inventory database.
@@ -29,7 +31,7 @@ public class InventoryContract {
     public static final String PATH_ITEMS = "items";
     public static final String PATH_COLLECTIONS = "collections";
 
-    public static final class LocationEntry implements BaseColumns {
+    public static final class ItemEntry implements BaseColumns {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_ITEMS).build();
@@ -50,11 +52,45 @@ public class InventoryContract {
         public static final String COLUMN_QUANTITY = "quantity";
         public static final String COLUMN_LOCATION = "location";
         public static final String COLUMN_DESC = "description";
+        // Stored as a string of the format yyyy-MM-dd
         public static final String COLUMN_EXPIRY = "expiry";
-        public static final String COLUMN_BATCH_NO = "batch_no";
+//        public static final String COLUMN_BATCH_NO = "batch_no";
 
-        public static Uri buildLocationUri(long id) {
+        public static Uri buildItemsUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+
+    /* Inner class that defines the table contents of the collections table */
+    public static final class CollectionsEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_COLLECTIONS).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_COLLECTIONS;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_COLLECTIONS;
+
+        public static final String TABLE_NAME = "collections";
+
+        // Column with the foreign key into the location table.
+        public static final String COLUMN_ITEM_KEY = "item_id";
+        // Quantity of the item in this collection
+        public static final String COLUMN_COLLEC_QUANTITY = "collec_quantity";
+
+
+        public static Uri buildCollectionsUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
+        /*
+            Student: This is the buildWeatherLocation function you filled in.
+         */
+        public static Uri buildCollection(String id) {
+            return CONTENT_URI.buildUpon().appendPath(id).build();
         }
     }
 }
