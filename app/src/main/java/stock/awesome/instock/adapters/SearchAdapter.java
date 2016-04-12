b6a522beacb25594642aed19c12ee8359cf825b8
kabir-plod <kabirk@live.com>
2016-04-12 23:05:45 +0800

    Autcompletify

--- app/src/main/java/stock/awesome/instock/adapters/SearchAdapter.java
+++ app/src/main/java/stock/awesome/instock/adapters/SearchAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import stock.awesome.instock.R;
 
+
 // From https://gist.github.com/fjfish/3024308
 public class SearchAdapter extends BaseAdapter implements Filterable {
 
