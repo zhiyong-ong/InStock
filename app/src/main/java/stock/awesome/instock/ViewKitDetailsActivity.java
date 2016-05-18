b3a0ea89386d98f8b4ebe873e4aeecad43c30e1f
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-05-19 01:52:19 +0800

    hide keyboard

--- app/src/main/java/stock/awesome/instock/ViewKitDetailsActivity.java
+++ app/src/main/java/stock/awesome/instock/ViewKitDetailsActivity.java
@@ -14,8 +14,8 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -133,6 +133,7 @@ public class ViewKitDetailsActivity extends AppCompatActivity {
                 barcodeInput.setFocusableInTouchMode(true);
                 barcodeInput.requestFocus();
                 Log.e("barcode input", "focused");
+
             }
         });
 
@@ -172,12 +173,8 @@ public class ViewKitDetailsActivity extends AppCompatActivity {
                 return true;
             }
         });
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
 
     }
 
